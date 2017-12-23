912839cd89e3955d9d68312a7687a45a5efb7266
dayfine <difancpa@gmail.com>
2017-12-23 17:02:08 -0500

    8.12

--- ch8-recursion-and-dynamic-programming/8.12-eight-queens.js
+++ ch8-recursion-and-dynamic-programming/8.12-eight-queens.js
@@ -1,20 +1,23 @@
 function eightQueens () {
-  const coll
+  const coll = []
 
-  const Queens = []
+  function buildBoard (Queens = []) {
+    if (Queens.length === 8) coll.push(Queens)
 
-
-   for (let i; i; i++)
-
-  function buildBoard (Queens=[]) {
-    if (Queens.length===8)  coll.push(Queens)
     const valid = (row, col) => Queens.every((cv, ci) =>
       cv !== col && Math.abs(ci - row) !== Math.abs(cv - col)
     )
 
+    let r = Queens.length
 
+    for (let c = 0; c < 8; c++) {
+      if (valid(r, c)) buildBoard([...Queens, c])
+    }
   }
 
+  buildBoard()
 
   return coll
 }
+
+eightQueens()
