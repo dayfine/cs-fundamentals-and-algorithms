2009d620697ded999f4fe40cc9fe1913540e3d90
dayfine <difancpa@gmail.com>
2018-04-14 23:15:03 -0400

    correct test file & multi

--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -6,4 +6,59 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// m = R0
+// n = R1
+// product = 0
+
+// LOOP:
+//   if n == 0 goto STOP
+//   product += m
+//   n -= 1
+//   goto LOOP
+// STOP:
+//   R2 = product
+
+// declartion variables: m * n = product
+@R0
+D=M
+
+@m
+M=D
+
+@R1
+D=M
+
+@n
+M=D
+
+@product
+M=0
+
+(LOOP)
+  @n
+  D=M
+  @STOP
+  D;JEQ // check n == 0
+
+  @product
+  D=M
+  @m
+  D=D+M
+  @product
+  M=D
+  @n
+  M=M-1
+  @LOOP
+  0;JMP
+
+(STOP)
+  @product
+  D=M
+  @R2
+  M=D
+  @product
+  M=0 // clean up afterwards
+
+(END)
+  @END
+  0;JMP
