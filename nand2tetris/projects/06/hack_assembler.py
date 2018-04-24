92dbd2eac927badbd3291d222930c5faa5e2ff5d
dayfine <difancpa@gmail.com>
2018-04-23 23:16:16 -0400

    assembler!

--- nand2tetris/projects/06/hack_assembler.py
+++ nand2tetris/projects/06/hack_assembler.py
@@ -1,4 +1,6 @@
-SYMBOL_TABLE = {
+from copy import copy
+
+SYMBOL_TABLE_TEMPLATE = {
     'SP': 0,
     'LCL': 1,
     'ARG': 2,
@@ -79,9 +81,8 @@ JUMP = {
 }
 
 
-def translate_a_instr(a_instr):
-    pass
-
+def translate_a_instr(address):
+    return '{0:b}'.format(address).zfill(16)
 
 def translate_c_instr(c_instr):
     """
@@ -91,7 +92,7 @@ def translate_c_instr(c_instr):
     find_eq = c_instr.find('=')
     find_semi = c_instr.find(';')
 
-    dest = c_instr[:find_eq]
+    dest = c_instr[:find_eq] if find_eq > -1 else ''
     jump = c_instr[find_semi+1:] if find_semi > -1 else ''
 
     if find_semi > -1:
@@ -102,37 +103,66 @@ def translate_c_instr(c_instr):
     compb = COMPUTATION[comp]
     destb = DESTINATION[dest]
     jumpb = JUMP[jump]
-
     return '111{}{}{}'.format(compb, destb, jumpb)
-
-
-class Code:
-    """
-    16-digit binary code machine instruction
-    """
-    def __init__(self):
-        pass
-
 
 class Assembler:
     def __init__(self):
         self.lines = []
         self.outputs = []
+        self.symbol_table = copy(SYMBOL_TABLE_TEMPLATE)
         self.next_var_addr = 16
 
+    def assemble(self, filepath):
+        self.load(filepath)
+        self.second_pass()
+        self.write_output(filepath)
+
     def load(self, filepath):
-        pass
+        with open(filepath, 'r') as f:
+            for line in f:
+                l = line.strip()
+                if not l or l.startswith('//'):
+                    continue
 
-    def parse(self):
-        pass
+                # first pass
+                if l.startswith('('):
+                    label = l[1:-1]
+                    self.symbol_table[label] = len(self.lines)
+                    continue
 
-    def first_pass(self):
-        pass
+                find_space = l.find(' ')
+                if find_space > -1:  # strip comments
+                    l = l[:find_space]
+                self.lines.append(l)
 
     def second_pass(self):
-        pass
+        for line in self.lines:
+            if line.startswith('@'):
+                # A-instruction
+                symbol = line[1:]
+                if symbol.isdigit():
+                    self.symbol_table[symbol] = int(symbol)
+                elif symbol not in self.symbol_table:
+                    self.symbol_table[symbol] = self.next_var_addr
+                    self.next_var_addr += 1
+                address = self.symbol_table[symbol]
+                self.outputs.append(translate_a_instr(address))
+            else:
+                # C-instruction
+                self.outputs.append(translate_c_instr(line))
 
+    def write_output(self, input_path):
+        output_path = input_path.replace('asm', 'hack')
+        with open(output_path, 'w') as f:
+            f.write('\n'.join(self.outputs))
 
+if __name__ == "__main__":
+    from sys import argv
+    from os.path import exists, join, dirname
+    script, filepath = argv
 
-
+    if not exists(filepath):
+        filepath = join(dirname(dirname(__file__)), filepath)
+    assembler = Assembler()
+    assembler.assemble(filepath)
 
