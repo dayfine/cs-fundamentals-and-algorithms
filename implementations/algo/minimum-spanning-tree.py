fef74c2baf327f7200c5bebb53a0c7e84f0c1c0a
dayfine <difancpa@gmail.com>
2018-07-10 22:33:53 -0400

    correct solution for MST

--- implementations/algo/minimum-spanning-tree.py
+++ implementations/algo/minimum-spanning-tree.py
@@ -1,10 +1,16 @@
+"""
+https://algs4.cs.princeton.edu/43mst/
+https://www.wikiwand.com/en/Disjoint-set_data_structure
+https://www.wikiwand.com/en/Kruskal%27s_algorithm
+"""
+
 class Graph:
     def __init__(self, num_vertices):
         self.num_vertices = num_vertices
         self.graph = []
 
     def add_edge(self, u, v, w):
-        self.graph.append(u, v, w)
+        self.graph.append([u, v, w])
 
     def find(self, ids, p):
         while p != ids[p]:
@@ -12,17 +18,17 @@ class Graph:
             p = ids[p]
         return p
 
-    def union(self, ids, ranks, x, y):
+    def union(self, ids, sizes, x, y):
         X = self.find(ids, x)
         Y = self.find(ids, y)
 
-        if ranks[X] < ranks[Y]:
-            ids[X] = Y
-        elif ranks[X] > ranks[Y]:
-            ids[Y] = X
-        else:
-            ranks[X] += 1
-            ids[Y] = X
+        if X == Y:
+            return
+
+        if sizes[X] < sizes[Y]:
+            X, Y = Y, X
+        ids[Y] = X
+        sizes[X] += sizes[Y]
 
     def minimum_spanning_tree(self):
         result = []
@@ -30,29 +36,35 @@ class Graph:
         # sorting by weight
         self.graph.sort(key=lambda item: item[2])
 
-        ids, ranks = [None] * self.num_vertices, [0] * self.num_vertices
+        ids, sizes = [], []
+        for i in range(self.num_vertices):
+            ids.append(i)
+            sizes.append(1)
 
         idx, edge = 0, 0
-
         while edge < self.num_vertices - 1:
             u, v, w = self.graph[idx]
             idx += 1
-            x = self.find(ids, u)
-            y = self.find(ids, v)
+            U = self.find(ids, u)
+            V = self.find(ids, v)
 
-            if x != y:
+            if U != V:
                 edge = edge + 1
-                result.append(self.graph[idx])
-                self.union(ids, ranks, x, y)
+                result.append([u, v, w])
+                print(result)
+                self.union(ids, sizes, U, V)
 
         return result
 
-g = Graph()
-g.addEdge(0, 1, 10)
-g.addEdge(0, 2, 6)
-g.addEdge(0, 3, 5)
-g.addEdge(1, 3, 15)
-g.addEdge(2, 3, 4)
+g = Graph(4)
+g.add_edge(0, 1, 10)
+g.add_edge(0, 2, 6)
+g.add_edge(0, 3, 5)
+g.add_edge(1, 3, 15)
+g.add_edge(2, 3, 4)
+
+MST = g.minimum_spanning_tree()
+print(MST)
 
 
 
